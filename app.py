440e0fe30b26e00bfbdf3534f15a47a0893d34e0
samiadail <samiadail12@gmail.com>
2026-06-27 11:44:22 -0300

    adicionando conexão com banco de dados

--- app.py
+++ app.py
@@ -4,9 +4,19 @@ from flask_cors import CORS
 from agno.models.openai import OpenAIChat
 from agno.agent import Agent
 from dotenv import load_dotenv
+from supabase import create_client
+import os
 
 #Leitura da chave de API
 load_dotenv()
+#Usando o getenv para pegar o arquivo específico
+supabase_url = os.getenv("SUPABASE_URL")
+#Usando o getenv para pegar o arquivo específico
+supabase_key = os.getenv("SUPABASE_KEY")
+#Criando a conexão com o banco de dados, passando a URL e a KEY.
+supabase = create_client(supabase_url,supabase_key)
+
+
 #Criar o nosso app
 app = Flask (__name__)
 #Habilitar o cors
@@ -36,6 +46,21 @@ def pergunta():
     pergunta = dados['pergunta']
     resposta = agente.run(pergunta)
     return jsonify({"resposta":resposta.content})
+
+
+#Criar a rota para servas
+@app.route("/reservar", methods=['POST'])
+def reservar():
+    dados = request.get_json()
+    nova_reserva = {
+        "nome":dados["nome"],
+        "email":dados["email"],
+        "check_in":dados["check_in"],
+        "tipo_quarto":dados["tipo_quarto"]
+    }
+    supabase.table("reservas").insert(nova_reserva).execute()
+    return jsonify({"mensagem":"Reserva realizada com sucesso!"})
+
 
 #Rodar o nosso app
 if __name__ == '__main__':
